43fa9e44f1d3233be2ca415f07fac04670aa5a98
Rico Klimpel <rico@tklimpel.de>
2019-06-24 13:51:21 +0200

    rename script file; added repo cloning

--- initproject.py
+++ initproject.py
@@ -6,17 +6,17 @@ import time
 from selenium import webdriver
 
 default_path = "/Users/rico/code/"
+github_username = "rklimpel"
 project_name = str(sys.argv[1])
 github_pw = str(sys.argv[2])
 
-def create_local_path():
-    print("Creating local folder ",project_name," in ",default_path,".")
-    #os.makedirs(default_path + project_name)
+print("Create new project '" + project_name + "' at '" + default_path + "'.")
 
 def create_remote_repository():
     browser = webdriver.Chrome("/usr/local/bin/chromedriver")
 
     #Github Login
+    print("Login to github.com...")
     browser.get("https://github.com/login")
     field_user_name = browser.find_elements_by_xpath("//*[@id='login_field']")[0]
     field_user_name.send_keys('rklimpel')
@@ -24,7 +24,9 @@ def create_remote_repository():
     field_user_pw.send_keys(github_pw)
     btn_login = browser.find_elements_by_xpath("//*[@name='commit']")[0]
     btn_login.click()
-    time.sleep(3)
+
+    #Create Repo
+    print("Creating repo on github.com...")
     browser.get("https://github.com/new")
     field_repo_name = browser.find_elements_by_xpath("//*[@name='repository[name]']")[0]
     field_repo_name.send_keys(project_name)
@@ -34,9 +36,14 @@ def create_remote_repository():
     toggle_readme.click()
     btn_create_repo = browser.find_element_by_css_selector("button.first-in-line")
     btn_create_repo.submit()
-    time.sleep(3)
+
+    browser.quit()
+
+def clone_repo():
+    print("Cloning repo to local machine...")
+    os.chdir(default_path)
+    os.system("git clone https://github.com/" + github_username + "/" + project_name)
 
 if __name__ == "__main__":
-    print("Creating new project ",project_name,"...")
-    create_local_path()
     create_remote_repository()
+    clone_repo()

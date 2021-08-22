c73f6b066bb3da2d3449616212b353a3398c7280
rklimpel <rico@tklimpel.de>
2021-08-22 10:43:44 +0200

    add config file

--- initproject.py
+++ initproject.py
@@ -1,31 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import sys
 import time
 from selenium import webdriver
+import configparser
 
-default_path = "/asdf/asdf/asdf"
-github_username = "asdf"
-github_password = "asdf"
+default_path = ""
+github_username = ""
+github_password = ""
 project_name = str(sys.argv[1])
 
-print("Create new project '" + project_name + "' at '" + default_path + "'.")
+def read_config_file():
+    global github_username, github_password, default_path
+    config = configparser.ConfigParser()
+    config.read('giteverything.ini')
+    github_username = config["DEFAULT"]["GithubUsername"]
+    github_password = config["DEFAULT"]["GithubPassword"]
+    default_path = config["DEFAULT"]["ProjectDirectory"]
+    print("Use Username " + github_username)
+    print("Use password " + github_password)
 
-def create_remote_repository():
-    browser = webdriver.Chrome("./chromedriver")
-
-    #Github Login
+def login_to_github():
+    global github_username, github_password, default_path
     print("Login to github.com...")
     browser.get("https://github.com/login")
     field_user_name = browser.find_elements_by_xpath("//*[@id='login_field']")[0]
-    field_user_name.send_keys('rklimpel')
+    field_user_name.send_keys(github_username)
     field_user_pw = browser.find_elements_by_xpath("//*[@id='password']")[0]
-    field_user_pw.send_keys(github_pw)
+    field_user_pw.send_keys(github_password)
     btn_login = browser.find_elements_by_xpath("//*[@name='commit']")[0]
     btn_login.click()
 
-    #Create Repo
+def create_remote_repository():
+    global github_username, github_password, default_path
     print("Creating repo on github.com...")
     browser.get("https://github.com/new")
     field_repo_name = browser.find_elements_by_xpath("//*[@name='repository[name]']")[0]
@@ -35,15 +43,22 @@ def create_remote_repository():
     toggle_readme = browser.find_elements_by_xpath("//*[@id='repository_auto_init']")[0]
     toggle_readme.click()
     btn_create_repo = browser.find_element_by_xpath("//button[contains(text(),'Create repository')]")
-    btn_create_repo.submit()
-
-    browser.quit()
+    btn_create_repo.submit() 
 
 def clone_repo():
+    global github_username, github_password, default_path
     print("Cloning repo to local machine...")
     os.chdir(default_path)
     os.system("git clone git@github.com:" + github_username + "/" + project_name + ".git")
 
 if __name__ == "__main__":
-    create_remote_repository()
+    print("Create new project '" + project_name + "' at '" + default_path + "'.")
+    read_config_file()
+    browser = webdriver.Chrome("./chromedriver")
+    try:
+        login_to_github()
+        create_remote_repository()
+    except:
+        print("Something failed")
+    browser.quit()
     clone_repo()
